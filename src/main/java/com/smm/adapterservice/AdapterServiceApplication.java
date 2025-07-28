2b6a34e7447ac9405a3c6ca1d0a53d8672a911f4
Samuel Maya Miles <smiles@gameaccount.com>
2025-07-28 17:38:03 +0200

    WIP: Create base project

---
+++ src/main/java/com/smm/adapterservice/AdapterServiceApplication.java
@@ -0,0 +1,12 @@
+package com.smm.adapterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdapterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdapterServiceApplication.class, args);
+    }
+}
